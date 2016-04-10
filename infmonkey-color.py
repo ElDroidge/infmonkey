0823ad89337c617a26e1e7f6568c6f3557e001a1
ElDroidge <manan995@gmail.com>
2016-04-10 15:52:58 +0530

    Added files via upload

---
+++ infmonkey-color.py
@@ -0,0 +1,30 @@
+#Infinite Monkey Theorem - Color Edition
+#by Droidge
+
+#Colour index:
+#30 - Black
+#31 - Red
+#32 - Green
+#33 - Yellow
+#34 - Blue
+#35 - Magenta
+#36 - Cyan
+#37 - White
+#38 - Crimson
+
+import sys
+import random
+
+colSelection = [34, 37, 31,] #Colours to use - refer to Colour Index
+totalChar = 320 #Number of characters
+lineBreak = 40 #Number of characters to break line after
+
+def randomColour():
+	foo = random.choice(colSelection) 
+	return foo
+
+print("(Try moving mousewheel if output seems to have halted)")
+for i in range(int(totalChar)):
+	if i % int(lineBreak) == 0:
+		print("\n")
+	sys.stdout.write('\033[1;' + str(randomColour()) + 'm#\033[1;m')
---
+++ infmonkey.py
@@ -0,0 +1,25 @@
+#py-infmonkey
+#A simple program that is inspired by the "infinite monkey theorem",
+#where a monkey hitting random keys on a typewriter keyboard
+#for an infinite amount of time will almost surely produce
+#a recognizable text, such as the complete works of William Shakespeare.
+#This version has the lowercase letters of the English Alphabet; 
+#along with a period/full stop, a comma, and a space. 
+
+import sys
+import string
+import random
+
+charSet = (tuple(string.ascii_lowercase) + (".", ",", " ")) #Declares the keys on the typewriter keyboard
+charTotal = raw_input("Number of keys to press: ") #Declares the number of keys in final document
+
+print("----------")
+
+for i in range(int(charTotal)):
+    sys.stdout.write(random.choice(charSet))
+
+print("\n----------")
+	
+
+
+
