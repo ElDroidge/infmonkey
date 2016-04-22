79888440b4a79f89c767995ba1897319792dcdd2
ElDroidge <manan995@gmail.com>
2016-04-22 22:48:47 +0530

    Update infmonkey-color.py

--- infmonkey-color.py
+++ infmonkey-color.py
@@ -2,32 +2,30 @@
 #by Droidge
 
 #Colour index:
-#30 - Black
-#31 - Red
-#32 - Green
-#33 - Yellow
-#34 - Blue
-#35 - Magenta
-#36 - Cyan
-#37 - White
-#38 - Crimson
+#0 - Black
+#1 - Red
+#2 - Green
+#3 - Yellow
+#4 - Blue
+#5 - Magenta
+#6 - Cyan
+#7 - White
+#8 - Crimson
 
 import sys
 import random
 
-import sys
-import random
-
-colSelection = [34, 37, 31,] #Colours to use - refer to Colour Index
-totalChar = 484 #Number of characters
-lineBreak = 22 #Number of characters to break line after
+colSelection = raw_input("Colours to use (refer to Colour Index inside code) (eg. '07' for 'Black' and 'White'): ")
+#print(list(colSelection))
+totalChar = raw_input("Number of 'pixels' to print: ")
+lineBreak =raw_input("Number of 'pixels' to break line after: ")
 
 def randomColour():
-	foo = random.choice(colSelection) 
+	foo = random.choice(list(colSelection)) 
 	return foo
 
-print("(Try moving mousewheel if output seems to have halted)")
+print("\n(Try moving mousewheel if output seems to have halted)")
 for i in range(int(totalChar)):
 	if i % int(lineBreak) == 0:
 		print("")
-	sys.stdout.write('\033[1;' + str(randomColour()) + 'm██\033[1;m')
+	sys.stdout.write('\033[1;' + '3' + str(randomColour()) + 'm██\033[1;m')
